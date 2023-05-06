db6872268468a889a3a04a79fdafe1c9ffe2367b
Alison Henrique Jonck <alisonjonck@gmail.com>
2023-05-06 17:07:20 -0300

    initial tests + documentation tests

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+
+/// Adds two integers.
+///
+/// # Examples
+///
+/// ```
+/// use app::sum_two_numbers;
+///
+/// assert_eq!(sum_two_numbers(3, 4), 7);
+/// ```
+pub fn sum_two_numbers(a: i32, b: i32) -> i32 {
+    a+b
+}
---
+++ tests/test_sum_two_numbers.rs
@@ -0,0 +1,7 @@
+// src/tests/test_sum_two_numbers.rs
+use app::sum_two_numbers;
+
+#[test]
+fn test_sum_two_numbers() {
+    assert_eq!(sum_two_numbers(10,12), 22);
+}
